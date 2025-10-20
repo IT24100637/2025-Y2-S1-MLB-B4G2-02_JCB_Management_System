4bbe1d623c1d27a6345d6571efb4510122deab8a
IT-24100603 <it24100603@my.sliit.lk>
2025-10-21 00:19:35 +0530

    ADD_JobStatus.java to job_management

---
+++ src/main/java/job_management/entity/JobStatus.java
@@ -0,0 +1,11 @@
+package com.example.demo.job_management.entity;
+
+public enum JobStatus {
+    SCHEDULED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED,
+    ON_HOLD,
+    OVERDUE,
+    PAUSED
+}
