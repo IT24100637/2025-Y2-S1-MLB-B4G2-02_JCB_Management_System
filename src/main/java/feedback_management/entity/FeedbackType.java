02faf898c3f7d760607968a93359669cb89b3b34
IT24100678 <it24100678@my.sliit.lk>
2025-10-21 07:00:35 +0530

    Add FeedbackType.java to feedback_management

---
+++ src/main/java/feedback_management/entity/FeedbackType.java
@@ -0,0 +1,12 @@
+package com.example.demo.feedback_management.entity;
+
+public enum FeedbackType {
+    COMPLAINT,
+    SUGGESTION,
+    COMPLIMENT,
+    BUG_REPORT,
+    FEATURE_REQUEST,
+    GENERAL_FEEDBACK,
+    SERVICE_RATING,
+    PRODUCT_RATING
+}
